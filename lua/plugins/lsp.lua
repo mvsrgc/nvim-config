ac69d4fd51a1563cd6d7559c8aeb46d05d2ffcea
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2023-11-08 21:12:49 -0500

    simplify lsp setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,31 +19,15 @@ return {
         config = function()
             local lsp_zero = require "lsp-zero"
 
-            lsp_zero.on_attach(function(client, bufnr)
+            lsp_zero.on_attach(function(_, bufnr)
                 lsp_zero.default_keymaps { buffer = bufnr }
                 lsp_zero.buffer_autoformat()
             end)
 
             require("mason").setup {}
             require("mason-lspconfig").setup {
-                ensure_installed = { "pyright" },
                 handlers = {
                     lsp_zero.default_setup,
-                    lua_ls = function()
-                        local lua_opts = lsp_zero.nvim_lua_ls()
-                        require("lspconfig").lua_ls.setup(lua_opts)
-                    end,
-                },
-            }
-
-            local cmp = require "cmp"
-            local cmp_format = lsp_zero.cmp_format()
-
-            cmp.setup {
-                formatting = cmp_format,
-                mapping = cmp.mapping.preset.insert {
-                    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-d>"] = cmp.mapping.scroll_docs(4),
                 },
             }
 
@@ -52,20 +36,17 @@ return {
             require("luasnip.loaders.from_vscode").lazy_load()
 
             cmp.setup {
-                preselect = "item",
-                completion = {
-                    completeopt = "menu,menuone,noinsert",
-                },
-                mapping = {
-                    ["<CR>"] = cmp.mapping.confirm { select = false },
-                    ["<Tab>"] = cmp_action.tab_complete(),
-                    ["<S-Tab>"] = cmp_action.select_prev_or_fallback(),
-                },
                 sources = {
                     { name = "path" },
                     { name = "nvim_lsp" },
                     { name = "buffer" },
                     { name = "luasnip" },
+                },
+                formatting = lsp_zero.cmp_format(),
+                mapping = cmp.mapping.preset.insert {
+                    ["<CR>"] = cmp.mapping.confirm { select = false },
+                    ["<Tab>"] = cmp_action.tab_complete(),
+                    ["<S-Tab>"] = cmp_action.select_prev_or_fallback(),
                 },
             }
         end,
