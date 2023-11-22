13b019da5bde6fd6ceef7d2b257b70a1c416f696
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2023-11-21 21:44:20 -0500

    Lsp thing

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -60,11 +60,10 @@ return {
         'stevearc/conform.nvim',
         opts = {
             format_on_save = {
-                timeout_ms = 500,
+                lsp_fallback = true,
             },
             formatters_by_ft = {
                 lua = { 'stylua' },
-                lsp_fallback = true,
             },
         },
     },
