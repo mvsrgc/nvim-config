c3f5c5fe31d777da6115e9ea272f6e5b2f0345cb
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2023-11-21 19:20:52 -0500

    Move telescope to core.lua

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -11,4 +11,10 @@ return {
             view_options = { show_hidden = true },
         },
     },
+    {
+        'nvim-telescope/telescope.nvim',
+        cmd = 'Telescope',
+        tag = '0.1.4',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+    },
 }
--- lua/plugins/telescope.lua
+++
@@ -1,8 +0,0 @@
-return {
-    {
-        'nvim-telescope/telescope.nvim',
-        cmd = 'Telescope',
-        tag = '0.1.4',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-    },
-}
